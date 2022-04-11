e8d1298486fb258eba95c900a6609e16a9439f9c
Jesse Reichman <Jesse.Reichman@noveris.com.au>
2022-04-11 10:45:46 +0930

    Removed dependencies for module.

--- source/CITools/CITools.psm1
+++ source/CITools/CITools.psm1
@@ -1,10 +1,6 @@
 <#
 #>
 
-#Requires -Modules @{"ModuleName"="Noveris.Logger";"RequiredVersion"="0.6.1"}
-#Requires -Modules @{"ModuleName"="Noveris.Version";"RequiredVersion"="0.5.2"}
-#Requires -Modules @{"ModuleName"="Noveris.GitHubApi";"RequiredVersion"="0.1.2"}
-
 ########
 # Global settings
 $InformationPreference = "Continue"
